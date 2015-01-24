89a348f5bd1b8a85e41e9970489880b6bcaea02a
jpailthorpe <jordanpailthorpe@gmail.com>
2015-01-24 17:31:24 -0500

    animation script fix

--- haunted-house-spouse/Assets/Scripts/Player/PlayerAnimation.cs
+++ haunted-house-spouse/Assets/Scripts/Player/PlayerAnimation.cs
@@ -5,11 +5,20 @@ public class PlayerAnimation : MonoBehaviour {
 	
 	public float maxSpeed = 10f;
 	bool facingRight = true;
+
+	Animator anim; 
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
 	
 	void FixedUpdate () 
 	{
 		float move = Input.GetAxis ("Horizontal");
-		
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
 		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 		
 		if (move > 0 && !facingRight)
