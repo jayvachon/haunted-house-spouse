04ced086446cf1579d88ee5f45c9d7b8657c6c76
Jay Vachon <jaylvachon@gmail.com>
2015-01-24 15:41:18 -0500

    add player and intractable objects

---
+++ haunted-house-spouse/Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+
+	bool colliding = false;
+	public string Hint {
+		get { return "Press spacebar to interact"; }
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.name == "Player") {
+			colliding = true;
+			OnEnter ();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.name == "Player") {
+			colliding = false;
+			OnExit ();
+		}
+	}
+
+	void Update () {
+		if (!colliding) return;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			OnInteract ();
+		}
+	}
+
+	protected void SetText (string content="") {
+		InteractableText.instance.Content = content;
+	}
+
+	protected virtual void OnEnter () {
+		SetText (Hint);
+	}
+	protected virtual void OnExit () {
+		SetText ();
+	}
+	protected virtual void OnInteract () {}
+}
---
+++ haunted-house-spouse/Assets/Scripts/UI/InteractableText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InteractableText : MonoBehaviour {
+
+	public Text text;
+	public string Content {
+		get { return text.text; }
+		set { text.text = value; }
+	}
+
+	public static InteractableText instance = null;
+
+	void Awake () {
+		if (instance == null)
+			instance = this;
+	}
+}
