06634e98ecf3a63987d6b995a14f013a51b2fc22
Jay Vachon <jaylvachon@gmail.com>
2015-01-25 11:43:11 -0500

    update camera

---
+++ haunted-house-spouse/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	public Transform player;
+	Vector3 startPosition;
+	float xMin = -2;
+	float xMax = 2;
+
+	void Start () {
+		startPosition = transform.position;
+	}
+
+	void Update () {
+		float x = player.position.x;
+		x = Mathf.Max (xMin, x);
+		x = Mathf.Min (xMax, x);
+		transform.position = new Vector3 (x, startPosition.y, startPosition.z);
+	}
+}
