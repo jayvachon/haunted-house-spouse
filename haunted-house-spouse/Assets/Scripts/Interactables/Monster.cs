2ebc6d3330e2e2fd1df93c1af83ab845e2439646
Jay Vachon <jaylvachon@gmail.com>
2015-01-24 22:10:04 -0500

    add hide spot

---
+++ haunted-house-spouse/Assets/Scripts/Interactables/Monster.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : Interactable {
+
+	void Awake () {
+		if (NetworkManager.Ghost) {
+			Visible = true;
+		} else {
+			Visible = false;
+		}
+		Options = new string[] {"APPEAR!"};
+	}
+
+	[RPC] void ReceiveContent (string newContent) {
+		if (newContent == "APPEAR!") {
+			
+		}
+	}
+}
